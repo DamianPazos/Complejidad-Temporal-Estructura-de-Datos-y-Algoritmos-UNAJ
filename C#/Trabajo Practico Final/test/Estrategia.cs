c3587b99f8814562b935bbcae1f97f4bab13dd37
Damian Pazos <damian_ariel_pazos>
2023-09-25 23:44:08 -0300

    Inicio de TPF

--- C#/Trabajo Practico Final/test/Estrategia.cs
+++ C#/Trabajo Practico Final/test/Estrategia.cs
@@ -22,11 +22,25 @@ namespace DeepSpace
 
 		public String Consulta3( ArbolGeneral<Planeta> arbol)
 		{
+			
 			return "Implementar";
 		}
 		
 		public Movimiento CalcularMovimiento(ArbolGeneral<Planeta> arbol)
 		{
+			// Buscar nodo del user
+			Planeta nodoUser = arbol.getDatoRaiz();
+
+			// Buscar nodo del bot
+			Planeta nodoBot = arbol.getDatoRaiz();
+			// Posibilidades
+
+			// 1. Estan en el mismo sub arbol y tiene mayor jerarquia
+
+			// 2. Estan en el mismo sub arbol y tiene menor jerarquia
+
+			// 3. Sub arboles distintos --> El bot debe desplazarse hacia la raiz
+			
 			//Implementar
 			
 			return null;
